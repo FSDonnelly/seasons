4f36a0734e18f3f0237756395006289ec6bfd8f2
FSDonnelly <donnelly_frank@yahoo.com>
2019-09-13 17:21:43 -0700

    refactor to func component using useState and useEffect hooks

--- src/index.js
+++ src/index.js
@@ -1,37 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 
-class App extends React.Component {
-    state = { lat: null, errorMessage: '' };
+const App = () => {
+  const [lat, setLat] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
 
-    componentDidMount() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => this.setState({ lat: position.coords.latitude }),
-            (err) => this.setState({ errorMessage: err.message })
-        );
-    };
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      position => setLat(position.coords.latitude),
+      err => setErrorMessage(err.message)
+    );
+  }, []);
 
-    renderContent() {
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div className="error-display">Error: {this.state.errorMessage}</div>
-        }
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else {
+    content = <Spinner message='Please accept location request' />;
+  }
 
-        if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat} />
-        }
-
-        return <Spinner message="Please accept location request" />;
-    };
-
-    render() {
-        return (
-            <div className="container">
-                {this.renderContent()}
-            </div>
-        )
-    };
+  return <div className='container'>{content}</div>;
 };
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// class App extends React.Component {
+//     state = { lat: null, errorMessage: '' };
+
+//     componentDidMount() {
+//         window.navigator.geolocation.getCurrentPosition(
+//             (position) => this.setState({ lat: position.coords.latitude }),
+//             (err) => this.setState({ errorMessage: err.message })
+//         );
+//     };
+
+//     renderContent() {
+//         if (this.state.errorMessage && !this.state.lat) {
+//             return <div className="error-display">Error: {this.state.errorMessage}</div>
+//         }
+
+//         if (!this.state.errorMessage && this.state.lat) {
+//             return <SeasonDisplay lat={this.state.lat} />
+//         }
+
+//         return <Spinner message="Please accept location request" />;
+//     };
+
+//     render() {
+//         return (
+//             <div className="container">
+//                 {this.renderContent()}
+//             </div>
+//         )
+//     };
+// };
+
+ReactDOM.render(<App />, document.getElementById('root'));
